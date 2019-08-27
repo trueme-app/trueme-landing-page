1a3941389b5f00dfc1b8e0189661ccf5ba276a51
struct78 <david@struct.com.au>
2019-08-27 21:12:33 +1000

    TMA-90 Add AWS amplify

---
+++ netlify-pre-build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+AWSCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":false,\
+\"accessKeyId\":\"${TRUEME_AWS_ACCESS_KEY_ID}\",\
+\"secretAccessKey\":\"${TRUEME_AWS_SECRET_ACCESS_KEY}\",\
+\"region\":\"${TRUEME_AWS_REGION}\"\
+}"
+AMPLIFY="{\
+\"envName\":\"${TRUEME_AWS_ENV}\"\
+}"
+PROVIDERS="{\
+\"awscloudformation\":${AWSCONFIG}\
+}"
+
+cd node_modules/@trueme/aws-amplify
+./node_modules/.bin/amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --yes
--- src/services/klaviyo.ts
+++ src/services/klaviyo.ts
@@ -1,21 +1,16 @@
-import axios from 'axios'
+import API from '@aws-amplify/api'
+import awsconfig from '@trueme/aws-amplify/aws-exports'
 
 const API_KEY = process.env.KLAVIYO_API_KEY
 const LIST_ID = process.env.KLAVIYO_LIST_ID
 
-export const setRegistration = async (email) => {
-  const response = await axios.post(`https://a.klaviyo.com/api/v2/list/${LIST_ID}/subscribe`, {
-      api_key: API_KEY,
-      profiles: [{
-        email,
-      }]
-    }, {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json',
-      }
-    }
-  )
+API.configure(awsconfig)
 
+export const setRegistration = async (email: string) => {
+  const response = await API.post('klaviyo', '/subscribe', {
+    body: {
+      email,
+    }
+  })
   return response
 }
