ebc44986b7a01cf0168270fe6044234e1fd6417a
struct78 <david@struct.com.au>
2019-08-27 23:15:49 +1000

    TMA-90 Update build script

--- netlify-pre-build.sh
+++ netlify-pre-build.sh
@@ -16,11 +16,11 @@ PROVIDERS="{\
 \"awscloudformation\":${AWSCONFIG}\
 }"
 
-cat > ./.aws << EOL
+cat > ~/.aws/credentials << EOL
 [netlify]
 region = $TRUEME_AWS_REGION
 aws_secret_access_key = $TRUEME_AWS_SECRET_ACCESS_KEY
 aws_access_key_id = $TRUEME_AWS_ACCESS_KEY_ID
 EOL
 
-AWS_SHARED_CREDENTIALS_FILE=./.aws ./node_modules/@trueme/aws-amplify/node_modules/.bin/amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --yes
+./node_modules/@trueme/aws-amplify/node_modules/.bin/amplify init --amplify ${AMPLIFY} --providers ${PROVIDERS} --yes
